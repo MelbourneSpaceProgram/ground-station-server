b6cae7ce01fabf12c5d0473501c94cbd40c78bd9
Toby Law <toby@tzcl.me>
2021-12-13 13:15:13 +1100

    Refactored app.py to use create_app

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from tracker.sat_tracker import SatelliteTracker
 
-app = Flask(__name__)
 
+def create_app(config=None):
+    app = Flask(__name__)
+    if config is None:
+        # app.config.from_pyfile()
+        pass
+    else:
+        app.config.from_mapping(config)
 
-@app.route("/")
-def index():
-    sat_tracker = SatelliteTracker()
+    @app.route("/")
+    @app.route("/index")
+    def index():
+        sat_tracker = SatelliteTracker()
 
-    sat_tracker.add_satellite(43013)
-    AOS, LOS = sat_tracker.passes[43013]
-    print(AOS)
-    print(LOS)
+        sat_tracker.add_satellite(43013)
+        AOS, LOS = sat_tracker.passes[43013]
+        print(AOS)
+        print(LOS)
 
-    return "<h1>Next Pass</h1><p>" + str(AOS) + "</p><p>" + str(LOS) + "</p>"
+        return "<h1>Next Pass</h1><p>" + str(AOS) + "</p><p>" + str(LOS) + "</p>"
+
+    return app
